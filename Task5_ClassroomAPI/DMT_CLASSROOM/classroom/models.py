98c4c597098c381a9561d18cb94558151393b3df
Dmitriy <astrovskiy.04@gmail.com>
2021-05-26 04:16:29 +0300

    Task5 classroom init

---
+++ Task5_ClassroomAPI/DMT_CLASSROOM/classroom/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    pass
+
+class Lecture(models.Model):
+    pass
+
+class Homework(models.Model):
+    pass
